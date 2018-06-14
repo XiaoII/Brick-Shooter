487d8c7c9cd4ef765c2fd832d9dd9627a8ded09e
XiaoII <32479440+XiaoII@users.noreply.github.com>
2018-06-14 15:46:14 +0100

    Up to Tutorial 10
    Up to the start or Tutorial 10
    https://www.youtube.com/watch?v=C7RaeIumgtM&list=PL4vbr3u7UKWpFlQGE6IlpzbBOzGpl1HBV&index=10

---
+++ Assets/Scripts/BrickHealthManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BrickHealthManager : MonoBehaviour {
+
+    public int brickHealth;
+    private Text brickHealthText;
+    private GameManager gameManager;
+
+	// Use this for initialization
+	void Start () {
+        gameManager = FindObjectOfType<GameManager>();
+        brickHealth = gameManager.level;
+        brickHealthText = GetComponentInChildren<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        brickHealthText.text = "" + brickHealth;
+        if (brickHealth <= 0)
+        {
+            //Destroy brick
+        }
+	}
+
+    void TakeDamage(int damageToTake)
+    {
+        brickHealth -= damageToTake;
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ball")
+        {
+            TakeDamage(1);
+        }
+    }
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public Transform[] spawnPoints;
+    public GameObject squareBrick;
+    public GameObject triangleBrick;
+    public int numberOfBricksToStart;
+    public int level;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        level = 1;
+
+        int numberOfBricksCreated = 0;
+
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            
+            
+
+                int brickToCreate = Random.Range(0,3);
+                if (brickToCreate == 0)
+                {
+                    Instantiate(squareBrick, spawnPoints[i].position, Quaternion.identity);
+                   
+                }
+                else if (brickToCreate == 1)
+                {
+                    Instantiate(triangleBrick, spawnPoints[i].position, Quaternion.identity);
+                   
+                }
+           
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
